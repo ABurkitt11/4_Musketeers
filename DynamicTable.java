7bdf267cc9b08d71c47f7d32e77cbe85b31bb05f
ABurkitt11 <116033437+ABurkitt11@users.noreply.github.com>
2023-02-04 19:47:31 -0800

    Update DynamicTable.java

--- DynamicTable.java
+++ DynamicTable.java
@@ -1,32 +1,28 @@
-public class DynamicTable 
-{
- private int capacity;
- private int itemAmount;
- 
- public DynamicTable(int newCapacity, int newItemAmount) 
- {
-   itemAmount = newItemAmount;
-   capacity = newCapacity;
- }
- 
- 
- public void setCapacity(int newCapacity) 
- {
-  capacity = newCapacity;
- }
- 
- public int getCapacity() {
-  return capacity;
- }
- 
- public void setItemAmount(int newItemAmount) 
- {
-  itemAmount = newItemAmount;
- }
- 
- public int getItemAmount() {
-  return itemAmount;
- }
- 
+public class DynamicTable {
+    private int capacity;
+    private int itemAmount;
+    private int[][] DTable;
+
+    public DynamicTable(int newCapacity, int newItemAmount) {
+        itemAmount = newItemAmount;
+        capacity = newCapacity;
+        DTable = new int[capacity][itemAmount];
+    }
+
+    public void setCapacity(int newCapacity) {
+        capacity = newCapacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setItemAmount(int newItemAmount) {
+        itemAmount = newItemAmount;
+    }
+
+    public int getItemAmount() {
+        return itemAmount;
+    }
+
 }
- 
