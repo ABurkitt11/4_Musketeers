6ac309394081e1ca322726983ca84fe6b217581c
joehelle <48190501+joehelle@users.noreply.github.com>
2023-02-04 22:37:44 -0800

    Dynamic table update v2.0.0!

--- DynamicTable.java
+++ DynamicTable.java
@@ -1,13 +1,15 @@
+import java.util.ArrayList;
+import java.math.*;
 public class DynamicTable {
     private int capacity;
     private int itemAmount;
     public int[][] DTable;
-
     public DynamicTable(int newCapacity, int newItemAmount) {
         itemAmount = newItemAmount;
         capacity = newCapacity;
         DTable = new int[capacity+1][itemAmount+1];
         
+    }
 
     public void setCapacity(int newCapacity) {
         capacity = newCapacity;
@@ -24,5 +26,39 @@ public class DynamicTable {
     public int getItemAmount() {
         return itemAmount;
     }
+    public void buildDynamicTable(ArrayList<Item> datastorage){
+     
+        for(int i = 0;i <= itemAmount;i ++){
+            for(int w = 0;w <= capacity; w ++){
+                    if(i==0){
+                        DTable[w][i] = 0;
+                    }
+                    else {
+                    int t;
+                    t = DTable[w][i-1];
+                    //ArrayList<Item> itemsUsed = new ArrayList<>();
+                    if(t < (datastorage.get(i).getPrice() + DTable[datastorage.get(i).getWeight()-w][i-1])){
+                        t = datastorage.get(i).getPrice() + DTable[datastorage.get(i).getWeight()-w][i-1];
+                        //itemsUsed.add(datastorage.get(i));
+                    }
+
+                    DTable[w][i] = t;
+                }
+        }
+    }
+    
+    
+}
+    public void countTheMoney(){
 
 }
+}
+/*for(int i = 0;i < itemAmount;i ++){
+    for(int w = 0;w < capacity; w ++){
+            int t;
+            t = DTable[w][i-1].getPrice;
+            if(t < (DTable[w][i].getPrice + Dtable[itemAmount-w][i-1].getPrice)){
+                t = DTable[w][i].getPrice + Dtable[itemAmount-w][i-1].getPrice;
+            }
+            DTable[w][i] = t;
+}*/
--- Main.java
+++ Main.java
@@ -9,15 +9,16 @@ public class Main {
     public static void main(String args[]) throws FileNotFoundException {
 //        Scanner sc = new Scanner(System.in);
 //        File f = new File(sc.nextLine());
-        File f = new File("D:\\java\\AI\\Knapsack\\src\\SampleKnapsackData.txt");
+        File f = new File("SampleKnapsackData.txt");
 //        sc.close();
 
 
         ArrayList<Item> dataStorage = new ArrayList<>();
         dataStorage = ReadFile.readfile(f);
+        DynamicTable dynamicTable = new DynamicTable(dataStorage.remove(0).getWeight(), dataStorage.size());
+        dynamicTable.buildDynamicTable(dataStorage);
 
-
-        Test.testClass(dataStorage, new DynamicTable(dataStorage.size(), dataStorage.remove(0).getWeight()));
+        //Test.testClass(dataStorage, new DynamicTable(dataStorage.size(), dataStorage.remove(0).getWeight()));
 
 
 
