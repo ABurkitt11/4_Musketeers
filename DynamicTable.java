978aed8213f102bd2e92762e08f5101ccc27866f
ABurkitt11 <116033437+ABurkitt11@users.noreply.github.com>
2023-02-08 17:02:01 -0800

    Update DynamicTable.java

--- DynamicTable.java
+++ DynamicTable.java
@@ -31,17 +31,13 @@ public class DynamicTable {
 
     public void buildDynamicTable(ArrayList<Item> datastorage) {
 
-        for (int w = 0; w <= capacity; w++) {
-        DTable[w][0] = 0; 
-        }
-
         for (int i = 1; i <= itemAmount; i++) {
             for (int w = 0; w <= capacity; w++) {
-                int t = DTable[w][i - 1];
+                int t = DTable[i - 1][w];
                 if (datastorage.get(i - 1).getWeight() <= w) {
-                    t = Math.max(t, datastorage.get(i - 1).getPrice() + DTable[w - datastorage.get(i - 1).getWeight()][i - 1]);
+                    t = Math.max(t, datastorage.get(i - 1).getPrice() + DTable[i - 1][w - datastorage.get(i - 1).getWeight()]);
                 }
-                DTable[w][i] = t;
+                DTable[i][w] = t;
             }
         }
 
