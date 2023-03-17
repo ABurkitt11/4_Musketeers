87b90376f8b7db6e5e7c7a66af463722ddba6c2a
joehelle <48190501+joehelle@users.noreply.github.com>
2023-03-17 08:11:52 -0700

    Add files via upload

--- Assignment2/Perceptron/main.py
+++ Assignment2/Perceptron/main.py
@@ -1,10 +1,12 @@
 
-
 from read import readdata
 import numpy as np
 
 def unit_step_func(x):
     return np.where(x > 0, 1, 0)
+
+def output_unit_step_func(x):
+    return np.where(x > 0, 'p', 'e')
 
 class Perceptron:
     def __init__(self, learning_rate = 0.1, max_epochs = 2):
@@ -12,7 +14,6 @@ class Perceptron:
         self.max_epochs = max_epochs
         self.weight = None
         self.bias = None
-        self.activation_func = unit_step_func
 
     def train_set(self, dataSet, yval):
 
@@ -27,7 +28,7 @@ class Perceptron:
         for _ in range(self.max_epochs):
             for index, row in dataSet.iterrows():
                 linear_output = np.dot(row, self.weight) + self.bias
-                y_predicted = self.activation_func(linear_output)
+                y_predicted = unit_step_func(linear_output)
 
 
                 update = self.learning_rate * (y_[index] - y_predicted)
@@ -37,16 +38,17 @@ class Perceptron:
 
     def predict(self, file_dict):
         linear_output = np.dot(file_dict, self.weight) + self.bias
-        y_predicted = self.activation_func(linear_output)
+        y_predicted = output_unit_step_func(linear_output)
         return y_predicted
 
 
 
 
-
+#trains perceptron using "MushroomData_8000.txt", then takes an input of the same format minus the edibility and predicts it for the mushroom(s)
 if __name__ == '__main__':
     y_val, file_dict = readdata("MushroomData_8000.txt",True)
     perceptron = Perceptron(0.1, 2)
     perceptron.train_set(file_dict, y_val)
-    test_data = readdata("MushroomData_Unknwon_100.txt", False)
-    print(perceptron.predict(file_dict.head(20)))
+    test_file = input("Enter test data file name ")
+    test_data = readdata(test_file, False)
+    print(perceptron.predict(test_data))
